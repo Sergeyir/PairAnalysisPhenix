5d9811afdad5c2d510982b8d6f5708ff76c828d6
Sergeyir <antsupov0124@gmail.com>
2025-03-12 12:16:04 +0300

    updating

--- GUI/DM.cpp
+++ GUI/DM.cpp
@@ -7,23 +7,31 @@
 void DM()
 {
 	gStyle->SetOptStat(0);
+   TDirectory::AddDirectory(kFALSE);
 
-   std::string runName;
-   CppTools::Print("Type in the run name");
-   std::cin >> runName;
+   CppTools::PrintInfo("List of runs in data/Real directory");
+   system("ls data/Real/");
+
+   CppTools::Print("Choose the run from the above ant type it in");
+   std::string runName = "Run14HeAu200";
+   //std::cout << ">> ";
+   //std::cin >> runName;
 
    const std::string realInputFileName = "data/Real/" + runName + "/SingleTrack/sum.root";
    CppTools::CheckInputFile(realInputFileName);
    TFile realInputFile(realInputFileName.c_str());
 
-   realInputFile.ls("Heatmap*");
+   CppTools::PrintInfo("List of heatmaps in " + realInputFileName + " file");
+   realInputFile.ls("Heatmap:*");
 
-   std::string detectorName;
    CppTools::Print("Type in the detector name");
+   std::string detectorName;
+   std::cout << ">> ";
    std::cin >> std::ws;
    std::getline(std::cin, detectorName);
 
    TH2D *realHist = static_cast<TH2D *>(realInputFile.Get(("Heatmap: " + detectorName).c_str()));
+
    if (!realHist) 
    {
       CppTools::PrintError("No histogram named Heatmap: " + detectorName + " in file " + realInputFileName);
@@ -33,9 +41,13 @@ void DM()
    
    GUIDistrCutter2D::AddHistogram(realHist);
    system(("mkdir -p data/Deadmaps/" + runName).c_str());
-   GUIDistrCutter2D::SetOutputFile("data/Deadmaps/" + runName + "/" + detectorName + ".txt");
 
-   GUIDistrCutter2D::Exec();
-	
+   const std::string outputCutsFileName = "data/Deadmaps/" + runName + "/" + detectorName + ".txt";
+   if (CppTools::FileExists(outputCutsFileName))
+   {
+      GUIDistrCutter2D::ReadCutAreas(outputCutsFileName);
+   }
+   GUIDistrCutter2D::SetOutputFile(outputCutsFileName);
+
 	gPad->AddExec("exec", "GUIDistrCutter2D::Exec()");
 }
